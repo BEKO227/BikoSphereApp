3544c4ad2cddf9e42ed39edfda7db91d7786ef3f
Youssef Hashish <yousfhashish0@gmail.com>
2025-09-05 00:00:38 +0300

    Update Post

--- src/Components/PostOptions/PostOptions.jsx
+++ src/Components/PostOptions/PostOptions.jsx
@@ -1,13 +1,16 @@
-import React, { use } from 'react'
+import React, { use, useRef } from 'react'
 import { initFlowbite } from 'flowbite'
 import { useEffect } from 'react';
 import axios from 'axios';
 import toast from 'react-hot-toast';
+import { useForm } from 'react-hook-form';
 
 export default function PostOptions({Post_id}) {
     useEffect(() => {
-        initFlowbite();
+        initFlowbite(); 
     }, []);
+    let fileInput = useRef()
+    let {handleSubmit , register} = useForm()
     async function handleDelete(){
         let {data} = await axios.delete(`https://linked-posts.routemisr.com/posts/${Post_id}`,
         {
@@ -25,9 +28,25 @@ export default function PostOptions({Post_id}) {
             toast.error(data.message)
         }
     }
-    // async function handleEdit(){
-    //     console.log("edit")
-    // }
+    async function handleEdit(obj){
+        let formData = new FormData()
+        formData.append('body', obj.body)
+        formData.append('image', fileInput.current.files[0])
+        let {data} = await axios.put(`https://linked-posts.routemisr.com/posts/${Post_id}`,formData,
+        {
+            headers:{
+                token : localStorage.getItem('token')
+            }
+        })
+        if (data.message == "success"){
+            toast.success('Post Edited Successfully')
+            setTimeout(() => { 
+                window.location.reload();
+            }, 1500);
+        }else{
+                toast.error(data.message)
+        }
+    }
   return (
     <div>
       <button id="dropdownMenuIconHorizontalButton" data-dropdown-toggle={"dropdown"+Post_id} className="text-gray-400 hover:text-gray-600 dark:hover:text-gray-300 p-1 rounded"
@@ -49,36 +68,44 @@ export default function PostOptions({Post_id}) {
         </div>
         <>
             {/* <!-- Main modal --> */}
-            <div id="default-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full  max-h-full">
+            <div id="default-modal" tabIndex="-1" aria-hidden="true" className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
                 <div className="relative p-4 w-full max-w-2xl max-h-full">
                     {/* <!-- Modal content --> */}
                     <div className="relative bg-white rounded-lg shadow-sm dark:bg-gray-700">
                         {/* <!-- Modal header --> */}
+                        <form onSubmit={handleSubmit(handleEdit)}>
                         <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600 border-gray-200">
                             <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-                                Terms of Service
+                                Edit Post
                             </h3>
                             <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal">
                                 <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
                                 </svg>
                                 <span className="sr-only">Close modal</span>
                             </button>
                         </div>
                         {/* <!-- Modal body --> */}
                         <div className="p-4 md:p-5 space-y-4">
-                            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                                With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.
-                            </p>
-                            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                                The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.
-                            </p>
+                            <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Post Text</label>
+                            <textarea id="message" {...register('body')} rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
+                            <div className="flex items-center justify-center w-full">
+                                <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-32 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600">
+                                    <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                                        <svg aria-hidden="true" className="mb-3 w-10 h-10 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
+                                        <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
+                                        <p className="text-xs text-gray-500 dark:text-gray-400">PNG, JPG or GIF (MAX. 800x400px)</p>
+                                    </div>
+                                    <input id="dropzone-file" ref={fileInput} type="file" className="hidden" />
+                                </label>
+                            </div>
                         </div>
                         {/* <!-- Modal footer --> */}
                         <div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
-                            <button data-modal-hide="default-modal" type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I accept</button>
+                            <button data-modal-hide="default-modal" type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Update Post</button>
                             <button data-modal-hide="default-modal" type="button" className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Decline</button>
                         </div>
+                        </form> 
                     </div>
                 </div>
             </div>
